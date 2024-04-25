ab5864ff5ebd2985db7b79fe629167aa7841476d
Faruq-Hameed <faruqhameed1@gmail.com>
2024-04-25 21:45:56 +0100

    added strings

--- basics/strings.py
+++ basics/strings.py
@@ -38,3 +38,6 @@ if ('best' in txt):
 # To check if a certain phrase or character is NOT present in a string, we can use the keyword not in.
 
 print('hal' not in a) # true
+
+if 'freed' not in txt:
+    print('freed is not there')
