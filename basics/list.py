80ce0559144946b69ecb09333dd216e3a54cad78
Faruq Hameed <faruqhameed1@gmail.com>
2024-05-07 22:00:31 +0100

    started list in py

---
+++ basics/list.py
@@ -0,0 +1,20 @@
+# Python Lists
+
+# Lists are used to store multiple items in a single variable(like an array in Js.).
+
+'''
+Lists are one of 4 built-in data types in Python used to 
+store collections of data, the other 3 are Tuple, Set, 
+and Dictionary, all with different qualities and usage.
+Lists are created using square brackets:
+'''
+myList= ['apple', 'orange', 'mango']
+print (myList) #['apple', 'orange', 'mango']
+
+# List Items
+# List items are ordered, changeable, and allow duplicate values.
+
+# List items are indexed, the first item has index [0], the second item has index [1] etc.
+
+myList[10] = 'pawpaw'
+print(myList)
