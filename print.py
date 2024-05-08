445ee48dd9ec691fa6714dd4f96b410d4aa2d206
Faruq Hameed <faruqhameed1@gmail.com>
2024-05-08 21:14:23 +0100

    restaged repo

--- print.py
+++ print.py
@@ -5,6 +5,24 @@ if 5 > 2:
     print("Five is greater than two!") 
 
 # In Python, variables are created when you assign a value to it:
-x = 'name'
+x = 'name' #another comment
 
 print(x)
+
+print('hello') # Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+# Python does not really have a syntax for multiline comments.
+
+# To add a multiline comment you could insert a # for each line:
+
+# Or, not quite as intended, you can use a multiline string.
+
+# Since Python will ignore string literals that are not assigned to a variable, 
+# you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+'hello world'
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
